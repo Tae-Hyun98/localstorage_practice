f0d7a50645b24c930fb7397f9a206c40fbc0af8d
Tae-Hyun <kt9811@naver.com>
2023-08-16 18:09:53 +0900

    카카오로그인 테스트

--- public/js/main.js
+++ public/js/main.js
@@ -22,8 +22,6 @@ mobileSide(); */
 const list = document.querySelector('.list ul');
 const listLi = document.querySelectorAll('.list ul li');
 
-
-
 function addList() {
   let addhtml = '';
   let count = 0;
@@ -159,3 +157,36 @@ function renderPost() {
 }
 
 renderPost();
+
+function loginWithKakao() {
+  Kakao.Auth.authorize({
+    redirectUri: '${REDIRECT_URI}',
+  });
+}
+
+// 아래는 데모를 위한 UI 코드입니다.
+displayToken()
+
+function displayToken() {
+  var token = getCookie('authorize-access-token');
+
+  if (token) {
+    Kakao.Auth.setAccessToken(token);
+    Kakao.Auth.getStatusInfo()
+      .then(function (res) {
+        if (res.status === 'connected') {
+          document.getElementById('token-result').innerText = 'login success, token: ' + Kakao.Auth.getAccessToken();
+        }
+      })
+      .catch(function (err) {
+        Kakao.Auth.setAccessToken(null);
+      });
+  }
+}
+
+function getCookie(name) {
+  var parts = document.cookie.split(name + '=');
+  if (parts.length === 2) {
+    return parts[1].split(';')[0];
+  }
+}
