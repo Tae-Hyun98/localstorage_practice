46d132298e3dfde50615bb614b84201859b4705e
Tae-Hyun <kt9811@naver.com>
2023-08-19 17:33:34 +0900

    카카오로그인테스트

--- js/kakao.js
+++ js/kakao.js
@@ -6,13 +6,40 @@ function loginWithKakao() {
   Kakao.Auth.authorize({
     redirectUri: 'http://127.0.0.1:5500/index.html',
     state: 'userme',
-  });
+  })
+  const token = getCookie('authorize-access-token');
+  if (token) {
+    Kakao.Auth.setAccessToken(token);
+    Kakao.Auth.getStatusInfo()
+      .then(function (res) {
+        if (res.status === 'connected') {
+          document.querySelector('.token-result').innerText = 'login success, token: ' + Kakao.Auth.getAccessToken();
+          document.querySelector('.logout').style.display = 'block';
+          
+        }
+      })
+      .catch(function (err) {
+        Kakao.Auth.setAccessToken(null);
+      });
+  }
+  
 }
 
+
+
+requestUserInfo()
 
 function requestUserInfo() {
   Kakao.API.request({
       url: '/v2/user/me',
+      data: {
+        grant_type: 'authorization_code',
+        //REST API키 입력
+        client_id: '73b344e027790c81033aa3a2801a1e84',
+        //redirect uri입력
+        redirect_uri: 'http://127.0.0.1:5500/index.html',
+        code: 'D3MJi6VGS_mEu0IDQJThZG6cYX1eEsRX6ZqfW7v-FW2SrdSHRO-8ZKXD0cM9GV2F21yQvAorDR4AA'
+      }
     })
     .then(function (res) {
       alert(JSON.stringify(res));
@@ -28,7 +55,7 @@ function requestUserInfo() {
 displayToken()
 
 function displayToken() {
-  const token = getCookie('authorize-access-token');
+
   if (token) {
     Kakao.Auth.setAccessToken(token);
     Kakao.Auth.getStatusInfo()
