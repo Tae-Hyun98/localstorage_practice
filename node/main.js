9a4392d315b49392ea1f76e5cae6d84d23319da4
Tae-Hyun <kt9811@naver.com>
2023-08-23 16:07:53 +0900

    test

--- node/main.js
+++ node/main.js
@@ -31,5 +31,50 @@ app.listen(4000, function () {
   console.log('서버 준비완료');
 }); */
 
-const arr = [1, 2, 3, 4];
-console.log(arr[0])
+const arr = [{
+    title: '테스트',
+    age: 20,
+    abc: '123'
+  },
+  {
+    title: '테스트2',
+    age: 30,
+    abc: '1234'
+  }
+];
+let object = [];
+object.push(arr)
+
+console.log('arr = ', arr) //객체배열
+console.log('stringify arr = ', JSON.stringify(arr))
+console.log('object push = ', ...object)
+
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  gender: "male",
+  interests: ["music", "skiing"],
+  bio: function () {
+    alert(
+      this.name[0] +
+      " " +
+      this.name[1] +
+      " is " +
+      this.age +
+      " years old. He likes " +
+      this.interests[0] +
+      " and " +
+      this.interests[1] +
+      ".",
+    );
+  },
+  greeting: function () {
+    const div = document.querySelector('.div1');
+    const p = document.createElement('p');
+    div.appendChild(p);
+
+    p.innerHTML = "Hi! I'm " + this.name[0] + ".";
+  },
+};
+console.log(person.name)
+person.greeting();
